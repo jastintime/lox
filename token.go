375f2a8da10ba6b3d74ad98374ac2c4a6aa9b44a
jastintime <130672671+jastintime@users.noreply.github.com>
2025-05-21 21:21:30 -0500

    Added string method to tokens

--- token.go
+++ token.go
@@ -6,6 +6,96 @@ import (
 
 type TokenType int
 
+func (t TokenType) String() string {
+	switch t {
+	case LeftParen:
+		return "LeftParen"
+	case RightParen:
+		return "RightParen"
+	case LeftBrace:
+		return "LeftBrace"
+	case RightBrace:
+		return "RightBrace"
+	case Comma:
+		return "Comma"
+	case Dot:
+		return "Dot"
+	case Minus:
+		return "Minus"
+	case Plus:
+		return "Plus"
+	case Semicolon:
+		return "Semicolon"
+	case Slash:
+		return "Slash"
+	case Star:
+		return "Star"
+
+		// One or two character tokens.
+	case Bang:
+		return "Bang"
+	case BangEqual:
+		return "BangEqual"
+	case Equal:
+		return "Equal"
+	case EqualEqual:
+		return "EqualEqual"
+	case Greater:
+		return "Greater"
+	case GreaterEqual:
+		return "GreaterEqual"
+	case Less:
+		return "Less"
+	case LessEqual:
+		return "LessEqual"
+
+		// Literals.
+	case Identifier:
+		return "Identifier"
+	case String:
+		return "String"
+	case Number:
+		return "Number"
+
+		// Keywords.
+	case And:
+		return "And"
+	case Class:
+		return "Class"
+	case Else:
+		return "Else"
+	case False:
+		return "False"
+	case Fun:
+		return "Fun"
+	case For:
+		return "For"
+	case If:
+		return "If"
+	case Nil:
+		return "Nil"
+	case Or:
+		return "Or"
+	case Print:
+		return "Print"
+	case Return:
+		return "Return"
+	case Super:
+		return "Super"
+	case This:
+		return "This"
+	case True:
+		return "True"
+	case Var:
+		return "Var"
+	case While:
+		return "While"
+	case EOF:
+		return "EOF"
+	}
+	return "Unknown Token"
+}
+
 const (
 	// Single-character tokens.
 	LeftParen TokenType = iota
@@ -31,7 +121,7 @@ const (
 	LessEqual
 
 	// Literals.
-	Identifiers
+	Identifier
 	String
 	Number
 
